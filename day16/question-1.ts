46e4cfb1828b74b784c7406378227448107a942b
hubert_i <leo.hubert@bodyguard.ai>
2024-12-16 14:48:27 +0100

    chore: add day 16

--- day16/question-1.ts
+++ day16/question-1.ts
@@ -13,7 +13,7 @@ function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function displayMap(map: string[][], paths: Position[]) {
+async function displayMap(map: string[][], paths: Position[], price: number) {
     const newMap = cloneDeep(map);
 
     for (let path of paths) {
@@ -22,58 +22,30 @@ async function displayMap(map: string[][], paths: Position[]) {
 
     console.clear();
     console.log(newMap.map(line => line.join('')).join('\n'));
-    console.log('Lower price: ', lowestPrice);
+    console.log('Lower price: ', lowestPrice - 1);
+    console.log('Price: ', price - 1);
 }
 
-function countStepsPrice(paths: Position[]) {
-    let price = 0
-    const pathByPosition = keyBy(paths, (path: Position) => `${path.x}-${path.y}`);
-
-    for (const curPath of paths) {
-        const upPosition = pathByPosition[`${curPath.x}-${curPath.y - 1}`];
-        const downPosition = pathByPosition[`${curPath.x}-${curPath.y + 1}`];
-        const leftPosition = pathByPosition[`${curPath.x - 1}-${curPath.y}`];
-        const rightPosition = pathByPosition[`${curPath.x + 1}-${curPath.y}`];
-
-        if (downPosition && leftPosition) {
-            price += 1001;
-        } else if (downPosition && rightPosition) {
-            price += 1001;
-        } else if (upPosition && leftPosition) {
-            price += 1001;
-        } else if (upPosition && rightPosition) {
-            price += 1001;
-        } else {
-            price += 1;
-        }
-
-    }
-
-    return price;
-}
 
 let lastShow: Date
 let lowestPrice = Infinity;
-async function findLowestPricePath(lines: string[][], map: Position[], position: Position, start: Position, end: Position, paths: Position[] = [], visitedPosition: Record<string, boolean> = {}) {
+async function findLowestPricePath(
+    lines: string[][],
+    map: Position[],
+    position: Position,
+    start: Position,
+    end: Position,
+    direction: 'up' | 'down' | 'left' | 'right' = 'right',
+    price: number = 0,
+    paths: Position[] = [],
+    visitedPosition: Record<string, boolean> = {}
+) {
     const pathByPosition = keyBy(map, (path: Position) => `${path.x}-${path.y}`);
 
     const upPosition = pathByPosition[`${position.x}-${position.y - 1}`];
     const downPosition = pathByPosition[`${position.x}-${position.y + 1}`];
     const leftPosition = pathByPosition[`${position.x - 1}-${position.y}`];
     const rightPosition = pathByPosition[`${position.x + 1}-${position.y}`];
-
-    if (position.x === end.x && position.y === end.y) {
-        console.log('end', countStepsPrice([...paths, position]))
-        const price = countStepsPrice([...paths, position]);
-        if (price < lowestPrice) {
-            lowestPrice = price;
-        }
-
-        return {
-            paths: [...paths, position],
-            price: price
-        };
-    }
 
     if (visitedPosition[`${position.x}-${position.y}`]) {
         return {
@@ -82,59 +54,82 @@ async function findLowestPricePath(lines: string[][], map: Position[], position:
         };
     }
 
-    if (!await findPathToTheEnd(lines, position, start, end, paths)) {
-        return {
-            paths: paths,
-            price: Infinity
-        };
-    }
+    // if (!await findPathToTheEnd(lines, position, start, end, paths)) {
+    //     return {
+    //         paths: paths,
+    //         price: Infinity
+    //     };
+    // }
 
     visitedPosition[`${position.x}-${position.y}`] = true;
     paths.push(position);
+    price++;
 
-    const currentPathPrice = countStepsPrice(paths);
-    if (currentPathPrice > lowestPrice) {
+    if (price > lowestPrice) {
         return {
             paths: paths,
             price: Infinity
         };
     }
 
-    if (!lastShow || (new Date().getTime() - lastShow.getTime()) > 1000) {
-        lastShow = new Date();
-        await displayMap(lines, paths);
+
+    if (position.x === end.x && position.y === end.y) {
+        if (price < lowestPrice) {
+            lowestPrice = price;
+        }
+        return {
+            paths: [...paths, position],
+            price: price
+        };
     }
 
-    let price = Infinity;
+    // if (!lastShow || (new Date().getTime() - lastShow.getTime()) > 1000) {
+    //     lastShow = new Date();
+        await displayMap(lines, paths, price);
+    // }
+
     let goodPath: Position[];
     if (upPosition) {
-        const newPath = await findLowestPricePath(lines, map, {x: position.x, y: position.y - 1}, start, end, cloneDeep(paths), cloneDeep(visitedPosition));
-        if (newPath.price <= price) {
-            price = newPath.price;
+        let newPrice = price;
+        if (direction === 'left' || direction === 'right') {
+            newPrice += 1000;
+        }
+        const newPath = await findLowestPricePath(lines, map, {x: position.x, y: position.y - 1}, start, end, 'up', newPrice, cloneDeep(paths), cloneDeep(visitedPosition));
+        if (newPath.price <= lowestPrice) {
             goodPath = newPath.paths;
         }
     }
 
     if (downPosition) {
-        const newPath = await findLowestPricePath(lines, map, {x: position.x, y: position.y + 1}, start, end, cloneDeep(paths), cloneDeep(visitedPosition));
-        if (newPath.price < price) {
-            price = newPath.price;
+        let newPrice = price;
+        if (direction === 'left' || direction === 'right') {
+            newPrice += 1000;
+        }
+        const newPath = await findLowestPricePath(lines, map, {x: position.x, y: position.y + 1}, start, end, 'down', newPrice, cloneDeep(paths), cloneDeep(visitedPosition));
+        if (newPath.price <= lowestPrice) {
             goodPath = newPath.paths;
         }
     }
 
     if (leftPosition) {
-        const newPath = await findLowestPricePath(lines, map, {x: position.x - 1, y: position.y}, start, end, cloneDeep(paths), cloneDeep(visitedPosition));
-        if (newPath.price < price) {
-            price = newPath.price;
+        let newPrice = price;
+        if (direction === 'up' || direction === 'down') {
+            newPrice += 1000;
+        }
+        const newPath = await findLowestPricePath(lines, map, {x: position.x - 1, y: position.y}, start, end, 'left', newPrice, cloneDeep(paths), cloneDeep(visitedPosition));
+        if (newPath.price <= price) {
             goodPath = newPath.paths;
         }
     }
 
     if (rightPosition) {
-        const newPath = await findLowestPricePath(lines, map, {x: position.x + 1, y: position.y}, start, end, cloneDeep(paths), cloneDeep(visitedPosition));
-        if (newPath.price < price) {
-            price = newPath.price;
+        let newPrice = price;
+        if (direction === 'up' || direction === 'down') {
+            newPrice += 1000;
+        }
+
+        const newPath = await findLowestPricePath(lines, map, {x: position.x + 1, y: position.y}, start, end, 'right', newPrice, cloneDeep(paths), cloneDeep(visitedPosition));
+        if (newPath.price <= lowestPrice) {
             goodPath = newPath.paths;
         }
     }
@@ -142,13 +137,13 @@ async function findLowestPricePath(lines: string[][], map: Position[], position:
     if (goodPath!) {
         return {
             paths: goodPath,
-            price: price
+            price: lowestPrice
         }
     }
 
     return {
-        paths: [],
-        price: Infinity
+        paths: paths,
+        price: price
     };
 
 }
@@ -215,14 +210,11 @@ async function run(file: string) {
         }
     }
 
-    const pathPositions = await findLowestPricePath(lines, map, {x: startPosition.x - 1, y: startPosition.y}, startPosition, endPosition);
+    const res = await findLowestPricePath(lines, map, {x: startPosition.x, y: startPosition.y}, startPosition, endPosition);
 
-    const price = countStepsPrice(pathPositions.paths);
+    await displayMap(lines, res.paths, res.price);
 
-    await displayMap(lines, pathPositions.paths);
-
-    console.log("Lowest price", price - 2);
-
+    console.log('Lowest price: ', lowestPrice - 1);
 
 }
 
